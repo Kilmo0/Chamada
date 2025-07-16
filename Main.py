45a41373be0be7dd5d65399242ccd6c49d8d2671
Kilmo <157428596+Kilmo0@users.noreply.github.com>
2025-07-16 11:34:48 -0300

    Beta V0.1.1
    Criação de botões, Tamanho das fontes interativas, Testes de Design do plano de fundo

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
 from tkinter import *
+import tkinter as tk
+from tkinter import font
 import sqlite3
 
 aluno = 'Kaua'
-root = Tk()
+root = tk.Tk()
+
+class relativefonts():
+    def relativeaptos(self, event):
+        largura = root.winfo_width()
+        newsize = max(8, int(largura / 40))
+        self.tituloresponsivo.configure(size=newsize)
+    def relativearial(self, event):
+        largura = root.winfo_width()
+        newsize = max(4, int(largura / 70))
+        self.relbotaoresp.configure(size=newsize)
+
 
 class mysql():
     def sqlconnect(self):
@@ -25,12 +38,13 @@ class mysql():
     # def registersystem():
 
 
-class APP(mysql):
+class APP(mysql, relativefonts):
     def __init__(self):
         self.root = root
         self.options()
         self.frames()
         self.label()
+        self.botoes()
         self.root.mainloop()
 
     def options(self):
@@ -38,11 +52,16 @@ class APP(mysql):
         self.root.geometry('800x600')
         self.root.minsize(width=800, height=600)
         self.root.maxsize(1920, 1080)
+        self.tituloresponsivo = font.Font(family='Aptos', size=20, weight='bold')
+        self.root.bind('<Configure>', self.relativeaptos)
+        self.relbotaoresp = font.Font(family='Arial', size=10, weight='bold')
+        self.root.bind('<Configure>', self.relativearial)
+        
 
     def frames(self):
         self.framebackground = Frame(self.root, background="#dedede")
         self.framebackground.place(relx=0, rely=0, relheight=1, relwidth=1)
-        self.frame1 = Frame(self.root, background='white')
+        self.frame1 = Frame(self.root, background="#e7ebfc")
         self.frame1.place(relx=0.02, rely=0.02, relheight=0.95, relwidth=0.5)
         self.frame2 = Frame(self.root, background='White')
         self.frame2.place(relx=0.5, rely=0.02, relheight=0.95, relwidth=0.48)
@@ -50,7 +69,18 @@ class APP(mysql):
         self.centerline.place(relx=0.49, rely=0.02, relheight=0.95, relwidth=0.03)
 
     def label(self):
-        self.labelregister = Label(self.frame1, text='Registrar Aluno', fg='#1A2C56', font=('Aptos', 20))
-        self.labelregister.place(relx=0.4, rely=0.07, relwidth=0.9, relheight=0.9)
+        self.labelregister = Label(self.frame1, text='Registrar Aluno', fg='#1A2C56', bg='#e7ebfc', font=self.tituloresponsivo)
+        self.labelregister.place(relx=0.1, rely=0.02, relwidth=0.7, relheight=0.2)
+        self.labelchamada = Label(self.frame2, text='Chamada', fg='#1A2C56', bg='#ffffff', font=self.tituloresponsivo)
+        self.labelchamada.place(relx=0.2, rely=0.02, relwidth=0.7, relheight=0.2)
+
+    def botoes(self):
+        self.botaoregistraraluno = Button(self.frame1, text='Registrar Aluno', background="#2857bd", fg='white', relief='groove',
+                                           borderwidth=4,font=self.relbotaoresp)
+        self.botaoregistraraluno.place(relx=0.1, rely=0.3, relheight=0.1, relwidth=0.75)
+        self.botaocriarturma = Button(self.frame1, text='Criar Turma', background='#2857bd', fg='white', relief='groove', 
+                                      borderwidth=4, font=self.relbotaoresp)
+        self.botaocriarturma.place(relx=0.1, rely=0.42, relwidth=0.75, relheight=0.1)
+
 
 APP()
